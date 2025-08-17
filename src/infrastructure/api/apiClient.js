5dfc04326c5100446e3e3e4efc153ce1040bfe4d
youngsuk <ys@gcnco.com>
2025-08-17 23:01:14 +0900

    domain

--- src/infrastructure/api/apiClient.js
+++ src/infrastructure/api/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance with default config
 const apiClient = axios.create({
-  baseURL: 'ttp://ec2-3-39-238-189.ap-northeast-2.compute.amazonaws.com:8080/api', // Adjust this to match your backend URL
+  baseURL: 'http://ec2-3-39-238-189.ap-northeast-2.compute.amazonaws.com:8080/api', // Adjust this to match your backend URL
   headers: {
     'Content-Type': 'application/json',
   },
